ceb28f927b04ec0696ad874fbee204652fe634af
Noémie Landry-Boisvert <noemie.landry-boisvert@usherbrooke.ca>
2019-01-20 15:50:41 -0500

    Fixed the HTML tags, added error handling and cleaned up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,26 +5,52 @@ import WasteWizard from '../../services/WasteWizard';
 import Search from '../Search/Search';
 import ItemList from '../ItemList/ItemList';
 
+/**
+ * App component:
+ * Base component of the application. It contains all the others!
+ * It is responsible for:
+ * 1 - Init the WasteWizard
+ * 2 - Handling errors when initiating the WasteWizard
+ * 3 - Keeping a list of searchResults
+ * 4 - Keeping a list of favorites
+ */
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchResults: [],
       favorites: [],
+      ready: 0, // 0: Not ready, 1: Ready, 2: Something went wrong!
     };
 
+    this.init = this.init.bind(this);
     this.toggleFavorite = this.toggleFavorite.bind(this);
     this.search = this.search.bind(this);
   }
 
   componentDidMount() {
-    WasteWizard.init();
+    this.init();
   }
 
   componentWillUnmount() {
     //
   }
 
+  // Init the WasteWizard and update the App ready state
+  init() {
+    WasteWizard.init()
+      .then(() => {
+        this.setState({
+          ready: 1,
+        });
+      }).catch(() => {
+        this.setState({
+          ready: 2,
+        });
+      });
+  }
+
+  // Toggle the favorite status on a waste and update the App favorites
   toggleFavorite(title) {
     const wastes = WasteWizard.toggleFavorite(title);
 
@@ -33,6 +59,7 @@ class App extends React.Component {
     });
   }
 
+  // Perfrom a search and update the App searchResults
   search(keyword) {
     const wastes = WasteWizard.search(keyword);
 
@@ -42,27 +69,53 @@ class App extends React.Component {
   }
 
   render() {
-    const { searchResults, favorites } = this.state;
-    const appTitle = 'Toronto Waste Lookup';
-    const favTitle = 'Favorites';
+    const { searchResults, favorites, ready } = this.state;
 
+    const titleReact = (
+      <h1 className="app-title">
+        Toronto Waste Lookup
+      </h1>
+    );
+
+    // If there's at least 2 favorites, render a list of favorites
     const favReact = favorites.length > 1 ? (
       <div className="favorites">
         <h2 className="favorites-title">
-          {favTitle}
+          Favorites
         </h2>
         <ItemList items={favorites} toggleFavorite={this.toggleFavorite} />
       </div>
     ) : '';
 
+    let appReact = [];
+
+    // The App is not ready yet!
+    if (ready === 0) {
+      appReact = (
+        <span className="loading">
+            Loading...
+        </span>
+      );
+    // The App is ready, render the search bar, the search results and the favorites
+    } else if (ready === 1) {
+      appReact = [
+        <Search onSearch={this.search} />,
+        <ItemList items={searchResults} toggleFavorite={this.toggleFavorite} />,
+      ];
+    // Something went wrong!
+    } else {
+      appReact = (
+        <span className="error">
+          Something went wrong. <button type="button" onClick={this.init}>Retry</button>
+        </span>
+      );
+    }
+
     return (
       <div className="app">
-        <h1 className="app-title">
-          {appTitle}
-        </h1>
-        <Search onSearch={this.search} />
-        <ItemList items={searchResults} toggleFavorite={this.toggleFavorite} />
-        {favReact}
+        { titleReact }
+        { appReact }
+        { favReact }
       </div>
     );
   }
--- src/services/WasteWizard.js
+++ src/services/WasteWizard.js
@@ -1,21 +1,29 @@
 import axios from 'axios';
 import Waste from '../models/Waste';
 
-const wastes = [];
-let favorites = [];
+const wastes = []; // All the wastes
+let favorites = []; // Favorite wastes
 
+/**
+ * The WasteWizard is responsible of:
+ * 1 - Getting the data and creating a list of wastes
+ * 2 - Searching through that list
+ * 3 - Toggling the favorite status on wastes and keeping a list of favorite wastes
+ */
 const WasteWizard = {
+
+  // Init the WasteWizard by getting the data and creating a list of Waste
   init() {
     return new Promise((resolve, reject) => {
       axios.get('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000')
         .then((response) => {
           const { status } = response;
-
           if (status !== 200) {
             const error = new Error(`Request Failed. Status Code: ${status}`);
             console.error(error.message);
             reject();
           } else {
+            // Create waste object with the received data and put it in a list
             response.data.forEach((w) => {
               const waste = new Waste(w.title, w.keywords, w.category, w.body);
               wastes.push(waste);
@@ -30,12 +38,15 @@ const WasteWizard = {
     });
   },
 
+  // Search a keyword through the list of wastes and returns the search results
   search(keyword) {
     const results = [];
     const k = keyword.toLowerCase();
 
+    // Return empty results list when search input is clear
     if (k === '') return results;
 
+    // Search the keyword in the title and in the keywords
     wastes.forEach((w) => {
       if (w.title.toLowerCase().includes(k)) results.push(w);
       else if (w.keywords.toLowerCase().includes(k)) results.push(w);
@@ -44,10 +55,13 @@ const WasteWizard = {
     return results;
   },
 
+  // Toggle the favorite status on a waste and returns the updated favorties
   toggleFavorite(title) {
+    // Find the waste with having the given title and toggle its favorite status
     const waste = wastes.find(w => w.title === title);
     waste.toggleFavorite();
 
+    // Update the favorite wastes list
     if (waste.favorite) favorites.push(waste);
     else {
       favorites = favorites.filter(w => w.title !== title);
@@ -57,4 +71,5 @@ const WasteWizard = {
   },
 };
 
+Object.freeze(WasteWizard);
 export default WasteWizard;
